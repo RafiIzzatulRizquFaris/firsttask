09f1cd24a420ac322e010bc0aaaaea6d02493da8
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-04-08 18:00:39 +0700

    Initial commit

---
+++ aboutme.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'Nama          : Rafi Izzatul Rizqu Faris <br>';
+echo 'Kelas         : XI RPL 1 <br>';
+echo 'Asal Sekolah  : SMK TARUNA BHAKTI <br>';
+echo 'Alamat        : Jl. Pemancar II no. 1 Cisalak, Sukmajaya, Kota Depok <br>';
+
+?>
