b667b50e3d9876ae4d54afda2495334d4fa18ff4
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-04-08 18:24:09 +0700

    Update aboutme.php
    Delete Alamat
    Add HP & Email

--- aboutme.php
+++ aboutme.php
@@ -3,6 +3,7 @@
 echo 'Nama          : Rafi Izzatul Rizqu Faris <br>';
 echo 'Kelas         : XI RPL 1 <br>';
 echo 'Asal Sekolah  : SMK TARUNA BHAKTI <br>';
-echo 'Alamat        : Jl. Pemancar II no. 1 Cisalak, Sukmajaya, Kota Depok <br>';
+echo 'HP            : 082299189919 <br>';
+echo 'Email         : risqufaris@gmail.com <br>';
 
 ?>
